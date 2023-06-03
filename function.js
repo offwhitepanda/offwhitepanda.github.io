8780682a93130a8b2bced27a23c1a0bad478404c
offwhitepanda <hurleyint_09@yahoo.com>
2023-06-03 23:17:49 +0900

    Added some further information and cleaned up some code.

--- function.js
+++ function.js
@@ -8,28 +8,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add click event listeners to each image
     for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener('click', function() {
-        // Check if the clicked image is within the image enlarger
-        if (!imageEnlarger.contains(this)) {
-        // Get the source of the clicked image
-        const src = this.src;
-        // Set the source of the enlarged image
-        enlargedImage.src = src;
-        // Open the image enlarger
-        imageEnlarger.classList.remove('image-enlarger-closed');
-        imageEnlarger.classList.add('image-enlarger-open');
-        }
-    });
+        images[i].addEventListener('click', function() {
+            // Check if the clicked image is within the image enlarger
+            if (!imageEnlarger.contains(this)) {
+                // Get the source of the clicked image
+                const src = this.src;
+                // Set the source of the enlarged image
+                enlargedImage.src = src;
+                // Open the image enlarger
+                imageEnlarger.classList.remove('image-enlarger-closed');
+                imageEnlarger.classList.add('image-enlarger-open');
+                // Scroll to the image enlarger
+                imageEnlarger.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     }
 
     // Add click event listener to close the image enlarger
     imageEnlarger.addEventListener('click', function() {
-    // Close the image enlarger
-    imageEnlarger.classList.remove('image-enlarger-open');
-    imageEnlarger.classList.add('image-enlarger-closed');
+        // Close the image enlarger
+        imageEnlarger.classList.remove('image-enlarger-open');
+        imageEnlarger.classList.add('image-enlarger-closed');
     });
 });
-  
+
 
 
 
